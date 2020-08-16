9ca8c0bd28a11fd5c6f2d0b2ede4144539cea1f8
lgthunder <lgthundera@gmail.com>
2020-08-16 23:06:45 +0800

    send file

--- lib/src/main/java/test/java/com/github/monkeywie/proxyee/CacheManager.java
+++ lib/src/main/java/test/java/com/github/monkeywie/proxyee/CacheManager.java
@@ -1,24 +1,63 @@
 package test.java.com.github.monkeywie.proxyee;
 
+import com.example.lib.FileUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.DefaultFileRegion;
+import io.netty.channel.FileRegion;
 import io.netty.handler.codec.http.DefaultHttpContent;
 import io.netty.handler.codec.http.DefaultHttpRequest;
 import io.netty.handler.codec.http.DefaultHttpResponse;
 import io.netty.handler.codec.http.DefaultLastHttpContent;
 import io.netty.handler.codec.http.HttpMethod;
 import io.netty.handler.codec.http.HttpRequest;
+import io.netty.handler.codec.http.HttpResponse;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpVersion;
+import io.netty.handler.codec.http.LastHttpContent;
 
 public class CacheManager {
 
-    public boolean hasCache(Channel clientChannel, Object msg, boolean isHttp) {
+    private static final String CR = System.getProperty("line.separator");
+    private boolean isCacheOpen = false;
+
+    String contentType;
+
+    public boolean hasCache(Channel ctx, Object msg, boolean isHttp) {
 //        System.out.println(msg.getClass());
 //        System.out.println(msg.toString());
+        if (!isCacheOpen) return false;
+        String filePath = FileUtils.DIR + File.separator + "test.jpg";
         if (msg instanceof DefaultHttpRequest) {
 
             DefaultHttpRequest request = (DefaultHttpRequest) msg;
             if (request.method() == HttpMethod.GET) {
                 System.out.println(" GET :" + request.uri());
+                System.out.println(" GET  REQUEST :" + request.toString());
+                try {
+                    File file = new File(filePath);
+                    DefaultHttpResponse response = new DefaultHttpResponse(HttpVersion.HTTP_1_0, HttpResponseStatus.OK);
+                    response.headers().add("Content-Type", "image/jpg");
+//                    ctx.write(file + " " + file.length() + CR);
+                    ctx.write(response);
+                    RandomAccessFile randomAccessFile = new RandomAccessFile(filePath, "r");
+                    FileRegion region = new DefaultFileRegion(randomAccessFile.getChannel(), 0, randomAccessFile.length());
+                    ctx.write(region);
+                    ctx.writeAndFlush(CR);
+                    randomAccessFile.close();
+                    return true;
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
             }
 
         }
@@ -28,21 +67,28 @@ public class CacheManager {
     }
 
 
-    public void saveResponseAsCache(Channel clientChannel, ChannelHandlerContext ctx, Object msg) {
+    public void saveResponseAsCache(Channel clientChannel, ChannelHandlerContext proxy, Object msg) {
+        //client  L:/127.0.0.1:9999 - R:/127.0.0.1:59484
 
         if (msg instanceof DefaultHttpResponse) {
             DefaultHttpResponse response = (DefaultHttpResponse) msg;
-//            if (response. == HttpMethod.GET) {
-//                System.out.println(" GET :" + request.uri());
-//            }
+            System.out.println(" GET RESPONSE :" + response.toString());
+            String contentType = response.headers().get("Content-Type");
 
+//            System.out.println("client: " + clientChannel.toString() + "| proxy :" + proxy.channel() + "RESPONSE: ");
 
         }
 
-        if (msg instanceof DefaultLastHttpContent) {
-            DefaultLastHttpContent response = (DefaultLastHttpContent) msg;
+        if (msg instanceof DefaultHttpContent) {
+            System.out.println(" GET RESPONSE_CONTENT :" + msg.toString());
+//            DefaultHttpContent response = (DefaultHttpContent) msg;
         }
 
-        System.out.println("RESPONSE: " + msg.getClass());
+        if (msg instanceof LastHttpContent) {
+//            System.out.println(proxy.channel()+ "RESPONSE: " + msg.getClass());
+
+        }
+
+
     }
 }
